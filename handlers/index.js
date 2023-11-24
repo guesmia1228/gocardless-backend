8e5996418c4a29af9fa4a18929894319a2130c8d
Guesmia Abdelmadjid <guesmiaabdelmadjid6@gmail.com>
2023-11-24 15:39:26 -0600

    email

--- handlers/index.js
+++ handlers/index.js
@@ -1,4 +1,8 @@
+const Email = require('email-templates');
+const convertHTMLToPDF = require('pdf-puppeteer');
+const pug = require('pug');
 const { client } = require("../gocardless");
+const fs = require('fs');
 
 const checkout = async (req, res) => {
   try {
@@ -118,7 +122,113 @@ const paymentInfo = async (req, res) => {
 };
 
 const webhooks = async (req, res) => {
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 4));
+  const { events } = req.body;
+  for (let event of events) {
+    if (event.resource_type === "mandates" && event.action === 'created') {
+      const mandate = event.links.mandate;
+      const mandate_detail = await client.mandates.find(mandate);
+      const bank_account = await client.customerBankAccounts.find(mandate_detail.links.customer_bank_account);
+      const customer = await client.customers.find(mandate_detail.links.customer);
+
+      const mandateHTML = pug.renderFile("./template/mandate_pdf.pug", {
+        name: `${customer.given_name} ${customer.family_name}`,
+        account_number_ending: bank_account.account_number_ending,
+        account_holder_name: bank_account.account_holder_name,
+        account_type: bank_account.account_type,
+        date: mandate_detail.created_at.slice(0,10),
+        ip_address: customer.ip_address
+      });
+
+      fs.writeFileSync('mandate.html', mandateHTML);
+
+      convertHTMLToPDF(mandateHTML, (pdf) => {
+        fs.writeFileSync("./mandate.pdf", pdf);
+
+        const email = new Email({
+          message: {
+            from: 'test@example.com'
+          },
+          // uncomment below to send emails in development/test env:
+          send: true,
+          transport: {
+            host: "sandbox.smtp.mailtrap.io",
+            port: 587,
+            auth: {
+              user: "51b7978516f5cf",
+              pass: "4f78b0ced7ffe0"
+            },
+          }
+        });
+
+        email
+          .send({
+            template: 'debit-authorization',
+            message: {
+              to: customer.email,
+              attachments: [
+                {
+                  filename: "mandate.pdf",
+                  content: pdf
+                }
+              ]
+            },
+            locals: {
+              full_name: `${customer.given_name} ${customer.family_name}`,
+              ref: mandate_detail.reference,
+              account_number_ending: bank_account.account_number_ending,
+              account_type: bank_account.account_type,
+            },
+          })
+          .then(console.log)
+          .catch(console.error);
+      }, {}, { args: ["--no-sandbox"] });
+    } else if (event.resource_type === "payments" && event.action === 'created') {
+      const payment = await client.payments.find(event.links.payment);
+      const mandate_detail = await client.mandates.find(payment.links.mandate);
+      const bank_account = await client.customerBankAccounts.find(mandate_detail.links.customer_bank_account);
+      const customer = await client.customers.find(mandate_detail.links.customer);
+      const creditor = await client.creditors.find(payment.links.creditor);
+
+      const email = new Email({
+        message: {
+          from: 'test@example.com'
+        },
+        // uncomment below to send emails in development/test env:
+        send: true,
+        transport: {
+          host: "sandbox.smtp.mailtrap.io",
+          port: 587,
+          auth: {
+            user: "51b7978516f5cf",
+            pass: "4f78b0ced7ffe0"
+          },
+        }
+      });
+
+      email
+        .send({
+          template: 'debit-payment',
+          message: {
+            to: customer.email,
+            attachments: []
+          },
+          locals: {
+            full_name: `${customer.given_name} ${customer.family_name}`,
+            ref: mandate_detail.reference,
+            amount: (payment.amount / 100).toFixed(2),
+            product: payment.description,
+            creditor: creditor.name,
+            account_number_ending: bank_account.account_number_ending,
+            account_type: bank_account.account_type,
+            charge_date: new Date(payment.charge_date).toLocaleDateString('en-us', { year: "numeric", month: "long", day: "numeric" }),
+            created_date: new Date(payment.created_at).toLocaleDateString('en-us', { year: "numeric", month: "long", day: "numeric" }),
+          },
+        })
+        .then(console.log)
+        .catch(console.error);
+    }
+  }
 
   return res.json("okay");
 }
